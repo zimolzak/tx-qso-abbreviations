763179482e05404592c849cc835aa18760ac2721
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-09-17 08:57:45 -0500

    working comparison code abbr vs full.

---
+++ parse.py
@@ -0,0 +1,13 @@
+with open('TX_county_abbrevs.txt') as fh:
+    for line in fh:
+        try:
+            abbr, full = line.split()
+            # print(abbr, ',', full)
+        except ValueError:
+            # print(line, end='')
+            pass
+        first4 = line[0:4]
+        last4 = line[-5:-1]
+        if first4.lower() != last4.lower():
+            print(line, end='')
+        
